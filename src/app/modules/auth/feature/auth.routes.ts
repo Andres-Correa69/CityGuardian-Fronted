5194e01ffe28427047cf3715eca255d0b2ee57cb
Santiago Gamez Guerra <gamez.guerra18@gmail.com>
2025-05-14 20:08:10 -0500

    add view verification

--- src/app/modules/auth/feature/auth.routes.ts
+++ src/app/modules/auth/feature/auth.routes.ts
@@ -5,7 +5,18 @@ export const AUTH_ROUTES: Routes = [
     {
         path:'login',
         loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
-        
+    },
+    {
+        path:'register',
+        loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent),
+    },
+    {
+        path:'verification',
+        loadComponent: () => import('./code-verification/code-verification.component').then(m => m.CodeVerificationComponent),
+    },
+    {
+        path:'reset-password',
+        loadComponent: () => import('./reset-password/reset-password.component').then(m => m.ResetPasswordComponent),
     },
     {
         path:'**',
--- src/app/modules/auth/feature/login/login.component.ts
+++ src/app/modules/auth/feature/login/login.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,RouterLink,ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    usuario: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+
+  onSubmit(){
+
+    console.log(this.loginForm.value);
+    console.log("slkdjflsd");
+    // Redireccionar a una ruta
+    this.router.navigate(['/auth/verification']);
+  }
+
 
 }
