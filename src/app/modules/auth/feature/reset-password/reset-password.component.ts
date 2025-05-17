3d987967cfdb82ec2b24f1ddb782261a8ab2a4c7
Santiago Gamez Guerra <gamez.guerra18@gmail.com>
2025-05-17 02:46:29 -0500

    add reset-view

--- src/app/modules/auth/feature/reset-password/reset-password.component.ts
+++ src/app/modules/auth/feature/reset-password/reset-password.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.css'
 })
 export class ResetPasswordComponent {
+  resetForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.resetForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit() {
+    if (this.resetForm.valid) {
+      // Aquí iría la lógica de recuperación
+      console.log('Correo para recuperación:', this.resetForm.value.email);
+    } else {
+      this.resetForm.markAllAsTouched();
+    }
+  }
 }
