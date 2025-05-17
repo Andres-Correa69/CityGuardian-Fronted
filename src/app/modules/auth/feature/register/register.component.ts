8504e32732e7872632dfad14f89e2c1035587313
Santiago Gamez Guerra <gamez.guerra18@gmail.com>
2025-05-17 02:16:48 -0500

    add register view

--- src/app/modules/auth/feature/register/register.component.ts
+++ src/app/modules/auth/feature/register/register.component.ts
@@ -1,18 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent  {
-  currentYear: number = new Date().getFullYear();
+export class RegisterComponent {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.registerForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      telefono: ['', Validators.required],
+      direccion: ['', Validators.required]
+    });
+  }
 
+  onSubmit() {
+    if (this.registerForm.valid) {
+      // Aquí iría la lógica de registro
+      console.log('Datos de registro:', this.registerForm.value);
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
 
-
-  
+  onCancel() {
+    this.router.navigate(['/auth/login']);
+  }
 }
