de404a8b4183d11174ff93c7ef04cf2eabdf7d96
Santiago Gamez Guerra <gamez.guerra18@gmail.com>
2025-05-17 01:32:10 -0500

    add maps and report

--- src/app/modules/report/feature/report-index/report-index.component.ts
+++ src/app/modules/report/feature/report-index/report-index.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MapService } from 'src/app/modules/map/service/map.service';
 
 @Component({
   selector: 'app-report-index',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './report-index.component.css'
 })
 export class ReportIndexComponent {
+  private mapService = inject(MapService);
+  ngOnInit(): void {
+    this.mapService.crearMapa();
+   
+   
+    this.mapService.agregarMarcador().subscribe((marcador) => {
 
+      console.log(marcador.lat);
+      console.log(marcador.lng);
+      
+      /*
+      this.crearReporteForm.get('ubicacion')?.setValue({
+        latitud: marcador.lat,
+        longitud: marcador.lng,
+      });
+      */
+    });
+   
+   
+   }
+   
 }
