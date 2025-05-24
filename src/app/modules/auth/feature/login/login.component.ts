2a986660731d4c4f4b89bd585eb1c7bf60881731
Santiago Gamez Guerra <gamez.guerra18@gmail.com>
2025-05-23 19:22:30 -0500

    add loading

--- src/app/modules/auth/feature/login/login.component.ts
+++ src/app/modules/auth/feature/login/login.component.ts
@@ -1,21 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
+import { Component, inject, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { LoaderComponent } from 'src/app/shared/ui/loading/loader/loader.component';
+import { LoaderService } from 'src/app/shared/ui/loading/loader/loader.service';
 import { ModalDesignService } from 'src/app/shared/ui/modals/modal-design/modal-design.service';
+
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,RouterLink,ReactiveFormsModule],
+  imports: [CommonModule,RouterLink,ReactiveFormsModule,LoaderComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   private router = inject(Router);
   private fb = inject(FormBuilder);
   private modalService = inject(ModalDesignService);
+  private loaderService = inject(LoaderService);
 
   @ViewChild('twoFactorTemplate') twoFactorTemplate!: TemplateRef<any>;
 
@@ -23,6 +27,10 @@ export class LoginComponent {
     usuario: ['', Validators.required],
     password: ['', Validators.required]
   });
+
+  ngOnInit(): void {
+    // this.loaderService.showLoading();
+  }
 
 
   onSubmit(){
